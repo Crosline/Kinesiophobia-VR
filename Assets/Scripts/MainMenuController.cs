d023925fc329e310d7b3c8bdcdde9bd88f4f2d85
Crosline <cagkancaglayanel@gmail.com>
2021-12-02 00:21:50 +0300

    XR Integration and Main Menu v0.4

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject menuPanel;
+    public GameObject settingsPanel;
+    public GameObject adminPanel;
+
+    public Slider audioSlider;
+
+    public AudioMixer audioMixer;
+    public float masterVolume;
+
+    public AudioSource buttonSound;
+
+    void Awake() {
+        masterVolume = PlayerPrefs.GetFloat("MasterVolume", 0);
+        settingsPanel.SetActive(true);
+        audioSlider.value = masterVolume;
+        SetMasterVolume(masterVolume);
+        settingsPanel.SetActive(false);
+
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenSettings(bool isOpen) {
+        settingsPanel.SetActive(isOpen);
+        menuPanel.SetActive(!isOpen);
+    }
+
+    public void OpenAdminPanel(bool isOpen) {
+        adminPanel.SetActive(isOpen);
+        menuPanel.SetActive(!isOpen);
+    }
+
+    public void SetMasterVolume(float volume) {
+        masterVolume = volume;
+        audioMixer.SetFloat("MasterVolume", masterVolume);
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+    }
+
+    public void ButtonOnClickSound() {
+        buttonSound.Play();
+    }
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
