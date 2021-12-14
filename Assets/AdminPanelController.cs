762b58fe21f6ac85921880601fb49ec9c011c988
unknown <kvr@DESKTOP-OKA74LU.hacettepe.edu.tr>
2021-12-14 11:56:21 +0300

    Admin Panel

--- Assets/AdminPanelController.cs
+++ Assets/AdminPanelController.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdminPanelController : MonoBehaviour {
 	
-	public void UpdateVideoSelection() {
+	public Dropdown branch;
+	public Dropdown gender;
+	public Dropdown session;
+	
+	public void UpdateSelection() {
+		int b, g, s;
+		b = PlayerPrefs.GetInt("Branch", 0);
+		g = PlayerPrefs.GetInt("Gender", 0);
+		s = PlayerPrefs.GetInt("Session", 0);
+		branch.value = b;
+		gender.value = g;
+		session.value = s;
 		
 	}
+	
 	public void SelectBranch(int b) {
+		PlayerPrefs.SetInt("Branch", b);
+		UpdateSelection();
+	}
+	
+	public void SelectGender(int g) {
+		PlayerPrefs.SetInt("Gender", g);
+		UpdateSelection();
+	}
+	
+	public void SelectSession(int s) {
+		PlayerPrefs.SetInt("Session", s);
+		UpdateSelection();
+	}
+	
+	public void SelectVideo() {
 		
 	}
 	
